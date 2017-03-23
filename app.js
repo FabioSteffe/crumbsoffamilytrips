2094823717d414d18cb0f959d7326fbee6376097
unknown <fsteffenino@NCEL18087.iis.amadeus.net>
2017-03-23 11:53:16 +0530

    try routes

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-var app = angular.module("crumbs",[]);
+var app = angular.module("crumbs",['ui.router']);
+
+app.config([ '$stateProvider', '$urlRouterProvider', function( $stateProvider, $urlRouterProvider){
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: './pages/home.html'
+    });
+
+  $urlRouterProvider.otherwise('home');
+
+} ]);
 
 app.controller("CrumbsBodyCtrl", [ '$scope', function($scope){
 
